14f44b0d3ddb440c5b865d7647d799a1414e05ae
udosreis <ugo.dosreis@gmail.com>
2020-09-23 14:59:35 +0200

    Simple blogging setup

---
+++ application.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__, template_folder="templates")
+
+
+def load_posts():
+    posts = []
+    for file in os.listdir("posts"):
+        if file.endswith(".txt"):
+            with open("posts" + os.path.sep + file) as rf:
+                post = {}
+                post["title"] = rf.readline().rstrip("\n")
+                post["date"] = rf.readline().rstrip("\n")
+                post["content"] = []
+                paragraph = ""
+                for line in rf:
+                    if line == "\n":
+                        post["content"].append(paragraph)
+                        paragraph = ""
+                    else:
+                        paragraph += line.rstrip("\n")
+                post["content"].append(paragraph)
+                posts.append(post)
+    return posts
+
+
+posts = load_posts()
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", posts = posts)
+
+
+@app.route("/<date>/<title>")
+def post(date, title):
+    for post in posts:
+        if post["title"] == title and post["date"] == date:
+            return render_template("post.html", post = post)
+
+if __name__ == "__main__":
+    app.run()
