09498b9797487a79fa259a325eb29502d45550e2
udosreis <ugo.dosreis@gmail.com>
2020-09-25 21:31:08 +0200

    New styling for the whole blog

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__, template_folder="templates")
@@ -13,20 +14,24 @@ def load_posts():
                 doc = ""
                 post["title"] = rf.readline().rstrip("\n")
                 post["author"] = rf.readline().rstrip("\n")
-                post["date"] = rf.readline().rstrip("\n")
+                post["date"] = datetime.strptime(rf.readline().rstrip("\n"), "%Y-%m-%d")
                 post["content"] = []
-                paragraph = ""
+                
+                paragraph = {"text": "", "type": 0}
                 for line in rf:
                     if line == "\n":
                         post["content"].append(paragraph)
-                        paragraph = ""
+                        paragraph = {"text": "", "type": 0}
                     else:
-                        paragraph += line.rstrip("\n")
+                        paragraph["text"] += line.rstrip("\n")
                     doc += line.rstrip("\n") + " "
                 post["content"].append(paragraph)
+
                 post["readtime"] = round(len(doc.split()) / 275)
+                post["preview"] = doc[0:100]
+
                 posts.append(post)
-    return posts
+    return sorted(posts, key=lambda x: x["date"], reverse=True)
 
 
 posts = load_posts()
@@ -37,10 +42,10 @@ def home():
     return render_template("home.html", posts = posts)
 
 
-@app.route("/<date>/<title>")
-def post(date, title):
+@app.route("/<year>/<month>/<day>/<title>")
+def post(year, month, day, title):
     for post in posts:
-        if post["title"] == title and post["date"] == date:
+        if post["title"] == title and post["date"] == datetime(int(year), int(month), int(day)):
             return render_template("post.html", post = post)
 
 if __name__ == "__main__":
