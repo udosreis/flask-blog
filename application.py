5fcfb72829cb0e640c5cb834be87f01393c7af24
udosreis <ugo.dosreis@gmail.com>
2020-09-27 23:30:53 +0200

    New article and updated design

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="templates")
 
@@ -17,19 +17,43 @@ def load_posts():
                 post["date"] = datetime.strptime(rf.readline().rstrip("\n"), "%Y-%m-%d")
                 post["content"] = []
                 
-                paragraph = {"text": "", "type": 0}
+                img_count = 0
                 for line in rf:
-                    if line == "\n":
-                        post["content"].append(paragraph)
-                        paragraph = {"text": "", "type": 0}
+                    paragraph = {"text": "", "type": "br"}
+                    line = line.lstrip().rstrip()
+                    if line[:3] == "<p>":
+                        paragraph["text"] += line[3:]
+                        paragraph["type"] = "p"
+                    elif line[:4] == "<h1>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "h1"
+                    elif line[:4] == "<h2>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "h2"
+                    elif line[:4] == "<h3>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "h3"
+                    elif line[:4] == "<h4>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "h4"
+                    elif line[:4] == "<h5>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "h5"
+                    elif line[:4] == "<h6>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "h6"
+                    elif line[:4] == "<li>":
+                        paragraph["text"] += line[4:]
+                        paragraph["type"] = "li"
+                    if line[:3] == "img":
+                        paragraph["text"] += "post-images/" + file.rstrip(".txt") + "-" + str(img_count) + line[4:]
+                        paragraph["type"] = "img"
+                        img_count += 1
                     else:
-                        paragraph["text"] += line.rstrip("\n")
-                    doc += line.rstrip("\n") + " "
-                post["content"].append(paragraph)
-
-                post["readtime"] = round(len(doc.split()) / 275)
+                        doc += paragraph["text"] + " "
+                    post["content"].append(paragraph)
+                post["readtime"] = round((len(doc.split()) + 12 * img_count) / 240)
                 post["preview"] = doc[0:100]
-
                 posts.append(post)
     return sorted(posts, key=lambda x: x["date"], reverse=True)
 
